0307d2e2de2ee1239dd9ce7ff07398e166b502b8
jz8132543 <jz8132543@live.cn>
2022-08-10 01:22:30 +0800

    update sops

--- hosts/ams0/hardware-configuration.nix
+++ hosts/ams0/hardware-configuration.nix
@@ -5,17 +5,15 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
     device = "nodev";
-  };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/C957-9CB6"; fsType = "vfat"; };
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
-  boot = {
-    loader.grub.extraConfig = ''
+    extraConfig = ''
       serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
       terminal_input console
       terminal_output console
-    '';
+      '';
+  };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/C957-9CB6"; fsType = "vfat"; };
+  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+  boot = {
     initrd = {
       availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
       kernelModules = [ "nvme" ];
