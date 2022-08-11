91f396fb52ecbf5c44fc1d725af6da7f3dd49c38
jz8132543 <jz8132543@live.cn>
2022-08-11 18:32:03 +0800

    fix sin0

--- hosts/sin0/hardware-configuration.nix
+++ hosts/sin0/hardware-configuration.nix
@@ -7,30 +7,38 @@
     efiInstallAsRemovable = true;
     device = "/dev/vda";
   };
-  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/74dd579c-a377-487d-b8f7-bc7c6df13ba1";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/922E-54A6";
+      fsType = "vfat";
+    };
   boot = {
     initrd = {
-      availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+      availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "virtio_blk" ];
       kernelModules = [ "nvme" ];
     };
-    kernelParams = [ "console=ttyS0,115200n8" ];
   };
 
   networking = {
     useNetworkd = true;
+    useDHCP = false;
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
   systemd.network = {
     enable = true;
     networks = {
-      ens0 = {
-        matchConfig = { Name = "ens0"; };
+      ens3 = {
+        matchConfig = { Name = "ens3"; };
         address = [ "139.59.240.130/20" "2400:6180:0:d0::e88:d001/64" ];
         gateway = [ "139.59.240.1" "2400:6180:0:d0::1" ];
       };
-      ens1 = {
-        matchConfig = { Name = "ens1"; };
+      ens4 = {
+        matchConfig = { Name = "ens4"; };
         address = [ "10.104.0.2/20" ];
       };
     };
