36be5512129f2ed9ef46b060512993ca35177f06
jz8132543 <i@dora.im>
2022-08-24 00:30:39 +0800

    add neovim lua dir

--- users/profiles/neovim/default.nix
+++ users/profiles/neovim/default.nix
@@ -11,4 +11,14 @@
       vimdiffAlias = true;
     };
   };
+
+  home.file.neovim = {
+    source = ./lua;
+    target = "~/.config/nvim";
+    recursive = true;
+  };
+
+  home.packages = with pkgs; [
+    rnix-lsp
+  ];
 }
