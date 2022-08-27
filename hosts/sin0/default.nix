78e924f95ff3c32b3e7cdfb7bdcf17d9f961ef82
jz8132543 <i@dora.im>
2022-08-27 18:07:57 +0800

    test sin0 btrfs

--- hosts/sin0/default.nix
+++ hosts/sin0/default.nix
@@ -1,22 +1,60 @@
-{ lib, config, pkgs, modulesPath, suites, profiles, ... }: {
-  imports = suites.server
-    ++ (with profiles; [ services.acme services.traefik services.k3s ])
-    ++ (with profiles.users; [ tippy ]);
+let
+  btrfsSubvol = device: subvol: extraConfig: lib.mkMerge [
+    {
+      inherit device;
+      fsType = "btrfs";
+      options = [ "subvol=${subvol}" "compress=zstd" ];
+    }
+    extraConfig
+  ];
+  btrfsSubvolMain = btrfsSubvol "/dev/disk/by-uuid/48503952-3b27-48a2-abfd-5a07a0a87e28";
+in{
+  imports =
+    suites.server ++
+    (with profiles; [
+      services.acme
+      services.traefik
+      services.k3s
+    ]) ++ (with profiles.users; [
+      tippy
+    ]);
 
-  environment.systemPackages = with pkgs;
-    [
+  environment.systemPackages = with pkgs;[
 
-    ];
+  ];
 
   networking.hostName = "sin0";
-  boot.loader.grub.device = "/dev/vda";
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "virtio_blk" ];
   boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/136735fa-5cc1-470f-9359-ee736e42f844";
-    fsType = "ext4";
+  boot.loader.grub = {
+      enable = true;
+      version = 2;
+      device = "/dev/vda";
+    };
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [
+      "/dev/disk/by-uuid/48503952-3b27-48a2-abfd-5a07a0a87e28"
+    ];
   };
+  fileSystems."/" =
+      {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [ "defaults" "size=1G" "mode=755" ];
+      };
+    fileSystems."/persist" = btrfsSubvolMain "@persist" { neededForBoot = true; };
+    fileSystems."/var/log" = btrfsSubvolMain "@var-log" { neededForBoot = true; };
+    fileSystems."/nix" = btrfsSubvolMain "@nix" { neededForBoot = true; };
+    fileSystems."/boot" =
+      {
+        device = "/dev/disk/by-uuid/E3E1-2EE5";
+        fsType = "ext4";
+      };
+    swapDevices =
+      [{
+        device = "/dev/disk/by-uuid/6142d3f3-e44f-4a16-8d92-634e2ad033d9";
+      }];
 
   networking = {
     defaultGateway = "128.199.64.1";
@@ -26,33 +64,15 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          {
-            address = "128.199.121.90";
-            prefixLength = 18;
-          }
-          {
-            address = "10.15.0.5";
-            prefixLength = 16;
-          }
+          { address="128.199.121.90"; prefixLength=18; }
+          { address="10.15.0.5"; prefixLength=16; }
         ];
         ipv6.addresses = [
-          {
-            address = "2400:6180:0:d0::1223:1001";
-            prefixLength = 64;
-          }
-          {
-            address = "fe80::984c:faff:fee5:d166";
-            prefixLength = 64;
-          }
+          { address="2400:6180:0:d0::1223:1001"; prefixLength=64; }
+          { address="fe80::984c:faff:fee5:d166"; prefixLength=64; }
         ];
-        ipv4.routes = [{
-          address = "128.199.64.1";
-          prefixLength = 32;
-        }];
-        ipv6.routes = [{
-          address = "2400:6180:0:d0::1";
-          prefixLength = 128;
-        }];
+        ipv4.routes = [ { address = "128.199.64.1"; prefixLength = 32; } ];
+        ipv6.routes = [ { address = "2400:6180:0:d0::1"; prefixLength = 128; } ];
       };
 
     };
