2da56ddffafd36299856c49f5d0da9231c21dadd
jz8132543 <i@dora.im>
2022-08-27 18:24:18 +0800

    test sin0 btrfs

--- users/root/default.nix
+++ users/root/default.nix
@@ -5,7 +5,6 @@ let
   homeDirectory = "/home/${name}";
 in{
   users.users.root = {
-    home = homeDirectory;
     initialPassword = "$6$T8HN3fbLSFq/QvjD$jb4zGjl.V1EcYkQhP4GcIVRsLf8SJUs6Mx9NCO2jl43AtOZTBj4JQ0OJafA5/ZMRwakjW3FP5ImrYBh2z4k/V/" ;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHUUFSNsaiMVMRtDl+Oq/7I2yViZAENbApEeCsbLJnq"
