9b01ac15dafa0479788d578eafc959ef8a296d2f
jz8132543 <i@dora.im>
2022-08-28 22:47:55 +0800

    fix ssh directory

--- profiles/services/openssh/default.nix
+++ profiles/services/openssh/default.nix
@@ -21,8 +21,18 @@ in {
     '';
   };
 
-  environment.global-persistence.directories = [
-    "/etc/ssh"
-  ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
 
+  environment.global-persistence = {
+    files = [
+      # ssh daemon host keys
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+    ];
+    user.files = [
+      ".ssh/known_hosts"
+    ];
+  };
 }
