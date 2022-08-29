6e21189ad096f01e259be3141c710f228f46bdf8
jz8132543 <i@dora.im>
2022-08-29 18:33:22 +0800

    convert tyo0 to btrfs

--- hosts/tyo0/default.nix
+++ hosts/tyo0/default.nix
@@ -1,4 +1,18 @@
-{ config, pkgs, modulesPath, suites, profiles, ... }: {
+{ config, pkgs, modulesPath, suites, profiles, ... }: 
+
+let
+
+  btrfsSubvol = device: subvol: extraConfig: lib.mkMerge [
+    {
+      inherit device;
+      fsType = "btrfs";
+      options = [ "subvol=${subvol}" "compress=zstd" ];
+    }
+    extraConfig
+  ];
+
+  btrfsSubvolMain = btrfsSubvol "/dev/disk/by-uuid/c1f57d9c-07fe-4e03-8ad9-25b72130133b";
+in{
   imports = suites.server ++ (with profiles; [
     services.acme
     services.v2ray
@@ -14,17 +28,28 @@
   boot.initrd.availableKernelModules =
     [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ "nvme" "kvm-amd" ];
-  fileSystems."/" = {
-    device = "/dev/vda1";
-    fsType = "ext4";
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [
+      "/dev/disk/by-uuid/c1f57d9c-07fe-4e03-8ad9-25b72130133b"
+    ];
   };
-  swapDevices = [
-    { device = "/dev/vda2"; }
+
+  fileSystems."/" = btrfsSubvolMain "@root" { neededForBoot = true; };
+  fileSystems."/persist" = btrfsSubvolMain "@persist" { neededForBoot = true; };
+  fileSystems."/var/log" = btrfsSubvolMain "@var-log" { neededForBoot = true; };
+  fileSystems."/nix" = btrfsSubvolMain "@nix" { neededForBoot = true; };
+  fileSystems."/swap" = btrfsSubvolMain "@swap" { };
+  fileSystems."/boot" =
     {
-      device = "/swapfile";
-      size = 1024;
-    }
-  ];
+      device = "/dev/disk/by-uuid/630C-8974";
+      fsType = "vfat";
+    };
+  swapDevices =
+    [{
+      device = "/swap/swapfile";
+    }];
 
   system.stateVersion = "22.11";
 }
