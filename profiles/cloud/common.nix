823c3992eeb5f49627041c109f0fedc0afab71e4
jz8132543 <i@dora.im>
2022-10-12 17:22:02 +0800

    test EFI

--- profiles/cloud/common.nix
+++ profiles/cloud/common.nix
@@ -26,8 +26,9 @@ in
     btrfs subvol create /fsroot/@persist
     btrfs subvol create /fsroot/@swap
     btrfs subvol create /fsroot/@ROOT
-    mkdir -p /mnt/{boot/EFI,nix,persist}
+    mkdir -p /mnt/{boot/EFI,nix,persist,tmp}
     mount /dev/vda2 /mnt/boot/EFI
+    mount tmpfs -t tmpfs -o size=4G -o mode=1777 /tmp
     mount -o subvol=@nix,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/nix
     mount -o subvol=@persist,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/persist
     export NIX_STATE_DIR=$TMPDIR/state
