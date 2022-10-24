b41d7a93aa7e48e55ea4941c7848393e47bd3a8f
jz8132543 <i@dora.im>
2022-10-24 20:32:17 +0800

    store temporarily

--- nixos/common.nix
+++ nixos/common.nix
@@ -2,13 +2,16 @@
 
 {
   imports = [
-    inputs.home.nixosModules.home-manager
-    inputs.sops-nix.nixosModules.sops
-    inputs.nixos-cn.nixosModules.nixos-cn
-    inputs.nixos-cn.nixosModules.nixos-cn-registries
-    inputs.impermanence.nixosModules.impermanence
-    (lib._.importExportableModules ../modules)
-  ];
+    # (lib._.importExportableModules ../modules)
+  ] ++ (builtins.map (path: ./${path}) (builtins.attrNames (builtins.readDir ../modules)));
+
+  #modules = [
+  #  inputs.home.nixosModules.home-manager
+  #  inputs.sops-nix.nixosModules.sops
+  #  inputs.nixos-cn.nixosModules.nixos-cn
+  #  inputs.nixos-cn.nixosModules.nixos-cn-registries
+  #  inputs.impermanence.nixosModules.impermanence
+  #];
 
   nixpkgs.overlays = [
   ];
