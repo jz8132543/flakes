7d9c688bd233fcf85111d203ad137eb12f0beeb7
jz8132543 <i@dora.im>
2022-10-24 16:15:11 +0800

    store temporarily

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,16 +1,13 @@
 { pkgs, self, inputs, lib,  ... }: 
 
 {
-  inherit (lib._);
   imports = [
     inputs.home.nixosModules.home-manager
     inputs.sops-nix.nixosModules.sops
     inputs.nixos-cn.nixosModules.nixos-cn
     inputs.nixos-cn.nixosModules.nixos-cn-registries
     inputs.impermanence.nixosModules.impermanence
-
-    # self.nixosModules
-  # ] ++ lib._.mapModulesRec' ../modules import;
+    (lib._.importExportableModules ../modules)
   ];
 
   nixpkgs.overlays = [
