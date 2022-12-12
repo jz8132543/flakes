8a6705cdb94cdfeaad3b085f48203e9d5c95bd67
jz8132543 <i@dora.im>
2022-12-12 21:44:38 +0800

    sway: fix telegram

--- users/modules/graphical/sway/default.nix
+++ users/modules/graphical/sway/default.nix
@@ -29,7 +29,7 @@ lib.mkIf (nixosConfig.environment.graphical.enable && nixosConfig.environment.gr
       assigns = {
         "1" = [{ app_id = "Alacritty"; }];
         "2" = [{ app_id = "firefox"; }];
-        "3" = [{ app_id = "telegramdesktop"; }];
+        "3" = [{ app_id = "org.telegram.desktop"; }];
         "9" = [{ app_id = "qemu"; }];
         "10" = [{ class = "thunderbird"; }];
       };
