b914e22ebbdfcb579b46f7bcf956c4b122bbaf42
jz8132543 <i@dora.im>
2022-12-13 20:28:37 +0800

    test: update

--- profiles/services/v2ray/default.nix
+++ profiles/services/v2ray/default.nix
@@ -13,9 +13,9 @@
     };
     nginx = {
       enable = true;
-      virtualHosts."test.tippic.eu.org" = {
+      virtualHosts."test.dora.im" = {
         forceSSL = true;
-        useACMEHost = "tippic.eu.org";
+        useACMEHost = "dora.im";
         listen = [{
           addr = "0.0.0.0";
           port = 8444;
