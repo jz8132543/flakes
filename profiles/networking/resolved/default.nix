32cc147e8ef9301a9b6380d9970ae6ba5417eced
jz8132543 <i@dora.im>
2023-01-19 22:39:24 +0800

    network: use resolvconf to delete dns 192.168 & fe80

--- profiles/networking/resolved/default.nix
+++ profiles/networking/resolved/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkMerge [
   {
@@ -23,6 +23,11 @@ lib.mkMerge [
       "1.1.1.1"
       "9.9.9.9"
     ];
+    networking.resolvconf.extraConfig = ''
+      #!/bin/sh -e
+      ${pkgs.gnused}/bin/sed -i "/192.168/d" /etc/resolv.conf
+      ${pkgs.gnused}/bin/sed -i "/fe80/d" /etc/resolv.conf
+    '';
   }
   (lib.mkIf config.services.avahi.enable {
     services.resolved.extraConfig = ''
