e31a0032321d888d40f637b2050efd040c457b45
jz8132543 <i@dora.im>
2023-03-06 23:39:13 +0800

    update: flake

--- hosts/lax3/default.nix
+++ hosts/lax3/default.nix
@@ -3,19 +3,13 @@
 {
   imports = suites.server ++ (with profiles; [
     cloud
-    services.acme
-    services.traefik
   ]) ++ (with profiles.users; [ tippy ]);
 
   environment.systemPackages = with pkgs; [  ];
 
   # Hardware
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
-
   boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "virtio_scsi" "sr_mod" "virtio_blk" ];
   boot.kernelModules = [ "kvm-intel" ];
 
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
 }
