193ae356371b7e7618fb4250d0457bb2c9787c34
jz8132543 <i@dora.im>
2023-03-11 17:14:20 +0800

    test

--- components/cloud/image.nix
+++ components/cloud/image.nix
@@ -16,20 +16,18 @@ in
       '';
       nativeBuildInputs = [ gptfdisk btrfs-progs mount util-linux nixUnstable config.system.build.nixos-install dosfstools ];
     } ''
-    sgdisk -Z -n 1:0:+1M -n 2:0:+100M -n 3:0:0 -t 1:ef02 -t 2:EF00 -c 1:BOOT -c 2:EFI -c 3:NIXOS /dev/vda
+    sgdisk -Z -n 1:0:+1M -n 2:0:0 -t 1:er02 -c 1:bios_grub -c 2:NIXOS /dev/vda
     mknod /dev/btrfs-control c 10 234
     mkfs.vfat /dev/vda1
-    mkfs.vfat /dev/vda2
-    mkfs.btrfs /dev/vda3
-    mkdir /fsroot && mount /dev/vda3 /fsroot
-    btrfs subvol create /fsroot/@nix
-    btrfs subvol create /fsroot/@persist
-    btrfs subvol create /fsroot/@swap
-    btrfs subvol create /fsroot/@ROOT
+    mkfs.btrfs /dev/vda2
+    mkdir /fsroot && mount /dev/vda2 /fsroot
+    btrfs subvol create /fsroot/nix
+    btrfs subvol create /fsroot/persist
+    btrfs subvol create /fsroot/boot
     mkdir -p /mnt/{boot,nix,persist,tmp}
     mount /dev/vda2 /mnt/boot
-    mount -o subvol=@nix,compress-force=zstd,space_cache=v2 /dev/vda3 /mnt/nix
-    mount -o subvol=@persist,compress-force=zstd,space_cache=v2 /dev/vda3 /mnt/persist
+    mount -o subvol=nix,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/nix
+    mount -o subvol=persist,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/persist
     export NIX_STATE_DIR=$TMPDIR/state
     nix-store --load-db < ${db}/registration
     nixos-install --root /mnt --system ${toplevel} --no-channel-copy --no-root-passwd --substituters ""
