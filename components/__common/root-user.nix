11aa50c02b8a6b18b2252c1c77420402b27e552c
jz8132543 <i@dora.im>
2023-03-15 00:54:26 +0800

    fix: password

--- components/__common/root-user.nix
+++ components/__common/root-user.nix
@@ -4,7 +4,6 @@
   users.users.root = {
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = import /${path}/config/sshkeys.nix;
-    hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/
-";
+    hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/";
   };
 }
--- components/tippy.nix
+++ components/tippy.nix
@@ -5,8 +5,7 @@
     shell = pkgs.fish;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = import /${path}/config/sshkeys.nix;
-    hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/
-";
+    hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/";
   };
   sops.secrets.passwd.neededForUsers = true;
   security.sudo.wheelNeedsPassword = false;
