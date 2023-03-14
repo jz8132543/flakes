7a571195b7357635a809c3318522f010a7b9abcd
jz8132543 <i@dora.im>
2023-03-15 00:50:17 +0800

    fix: password

--- components/__common/root-user.nix
+++ components/__common/root-user.nix
@@ -4,6 +4,7 @@
   users.users.root = {
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = import /${path}/config/sshkeys.nix;
-    passwordFile = config.sops.secrets.passwd.path;
+    hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/
+";
   };
 }
