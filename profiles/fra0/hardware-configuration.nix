46021a07437876711018885d8b6145bdb54c10fe
jz8132543 <i@dora.im>
2023-03-28 22:01:09 +0800

    fix: fra0

--- profiles/fra0/hardware-configuration.nix
+++ profiles/fra0/hardware-configuration.nix
@@ -9,4 +9,11 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" ];
   boot.kernelModules = [ "kvm-amd" ];
   utils.disk = "/dev/sda";
+  networking = {
+    interfaces.ens3 = {
+      useDHCP = true;
+      ipv6.addresses = [{ address = "2a00:0f48:1003:25bc:0000:0000:0000:0001"; prefixLength = 64; }];
+      ipv6.routes = [{ address = "::"; prefixLength = 0; via = "2a00:0f48:1003:0000:0000:0000:0000:0001"; }];
+    };
+  };
 }
