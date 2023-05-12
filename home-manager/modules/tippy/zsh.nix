939e3cb00aff826026bf6f81fdb86b5569218d0e
jz8132543 <jz8132543@live.cn>
2023-05-12 16:39:12 +0000

    test: flake-parts

--- home-manager/modules/tippy/zsh.nix
+++ home-manager/modules/tippy/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.starship = {
     enable = true;
@@ -6,11 +6,29 @@
 
   programs.zsh = {
     enable = true;
-    history.path = "$HOME/.cache/zsh_history";
+    history.path = "$HOME/.local/share/zsh/zsh_history";
 
     enableVteIntegration = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
     enableCompletion = true;
+
+    shellAliases = {
+      ls = "${pkgs.exa}/bin/exa --icons";
+      tree = "${pkgs.exa}/bin/exa --tree --icons";
+      top = "${pkgs.bottom}/bin/btm";
+      cat = "${pkgs.bat}/bin/bat --style=plain";
+      fzf =
+        "${pkgs.fzf}/bin/fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'";
+      batdiff =
+        "git diff --name-only --relative --diff-filter=d | xargs ${pkgs.bat}/bin/bat --diff";
+      rg = "${pkgs.ripgrep}/bin/rg --no-ignore";
+      # scp = "time scp -Cpr -o Compression=yes";
+    };
+  };
+  home.persistence."/nix/persist/home/tippy" = {
+    directories = [
+      ".local/share/zsh"
+    ];
   };
 }
