86002bcf5f7084bf7047331e01515fd0dbcb8550
jz8132543 <jz8132543@live.cn>
2023-05-12 15:48:59 +0000

    test: flake-parts

--- nixos/modules/users/tippy/default.nix
+++ nixos/modules/users/tippy/default.nix
@@ -1,18 +1,27 @@
 { config, pkgs, ... }:
+let
+  name = "tippy";
+  homeDirectory = "/home/${name}";
+in
 {
-  users.users.tippy = {
+  users.users.${name} = {
     isNormalUser = true;
     shell = pkgs.fish;
     extraGroups = [ "wheel" ];
-    # openssh.authorizedKeys.keys = import /${path}/config/sshkeys.nix;
+    openssh.authorizedKeys.keys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHUUFSNsaiMVMRtDl+Oq/7I2yViZAENbApEeCsbLJnq";
     hashedPassword = "$6$0gRnTBQjBv9ipXZz$AEBVrBbWXgzZ0IICD1HVWeCwqELFe85.ePsOOdkvFM1E6/sKvQUUesvXhQN519Ud33RsqA3h5z.4luO8Jk4Ls/";
   };
   # sops.secrets.passwd.neededForUsers = true;
   security.sudo.wheelNeedsPassword = false;
-  environment.persistence."/nix/persist".users.tippy = {
-    directories = [
-      "source"
-      ".local/share/direnv"
-    ];
+  home-manager.users.${name} = { hmModules, ... }: {
+    imports = hmModules.${name}.all;
+    home.global-persistence = {
+      enable = true;
+      home = homeDirectory;
+      directories = [
+        "source"
+        ".local/share/direnv"
+      ];
+    };
   };
 }
