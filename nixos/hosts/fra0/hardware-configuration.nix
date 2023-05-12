70307bee7fa9a068b900c9e5ff3ed7809acf5722
jz8132543 <jz8132543@live.cn>
2023-05-12 14:03:19 +0000

    test: flake-parts

--- nixos/hosts/fra0/hardware-configuration.nix
+++ nixos/hosts/fra0/hardware-configuration.nix
@@ -13,18 +13,18 @@
   networking = {
     # useDHCP = false;
     # useNetworkd = true;
-    interfaces.ens18 = {
+    interfaces.eth0 = {
       useDHCP = true;
       ipv4.addresses = [{ address = "109.71.253.195"; prefixLength = 24; }];
       ipv6.addresses = [{ address = "2a0e:6a80:3:1e3::"; prefixLength = 64; }];
     };
     defaultGateway = {
       address = "109.71.253.1";
-      interface = "ens18";
+      interface = "eth0";
     };
     defaultGateway6 = {
       address = "fe80::1";
-      interface = "ens18";
+      interface = "eth0";
     };
   };
 }
