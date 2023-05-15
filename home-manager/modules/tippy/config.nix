c4ef57e764e15cca1099aaae0e9971ead7c86b45
jz8132543 <jz8132543@live.cn>
2023-05-15 15:07:22 +0000

    test: flake-parts

--- home-manager/modules/tippy/config.nix
+++ home-manager/modules/tippy/config.nix
@@ -3,17 +3,12 @@ let
   homeDirectory = "/home/tippy";
 in
 {
-  #sops.secrets.id_ed25519.neededForUsers = true;
-  #home.file.".ssh/id_ed25519".source = config.lib.file.mkOutOfStoreSymlink "${config.sops.secrets.id_ed25519.path}";
-  # sops.secrets.id_ed25519.path = ".ssh/id_ed25519";
+  sops.secrets."ssh/id_ed25519" = { };
+  home.file.".ssh/id_ed25519".source = config.lib.file.mkOutOfStoreSymlink "${config.sops.secrets."ssh/id_ed25519".path}";
   home.file.".ssh/id_ed25519.pub".source = pkgs.writeText "pub" ''
     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHUUFSNsaiMVMRtDl+Oq/7I2yViZAENbApEeCsbLJnq i@dora.im
   '';
   home.file."source/nvim".source = config.lib.file.mkOutOfStoreSymlink "${homeDirectory}/.config/nvim";
-  sops.secrets."ssh/id_ed25519" = {
-    # sopsFile = osConfig.sops-file.get "common.yaml";
-    path = ".ssh/id_ed25519";
-  };
   home.persistence."/nix/persist/home/tippy" = {
     allowOther = false;
   };
