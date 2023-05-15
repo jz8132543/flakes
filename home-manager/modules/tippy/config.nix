3cd2ffb4f0cde9013f7e2b91b0ac810a737239bd
jz8132543 <jz8132543@live.cn>
2023-05-15 12:29:33 +0000

    test: flake-parts

--- home-manager/modules/tippy/config.nix
+++ home-manager/modules/tippy/config.nix
@@ -1,4 +1,4 @@
-{ config, inputs, self, pkgs, ... }:
+{ config, osConfig, inputs, self, pkgs, ... }:
 let
   homeDirectory = "/home/tippy";
 in
@@ -10,6 +10,10 @@ in
     ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHUUFSNsaiMVMRtDl+Oq/7I2yViZAENbApEeCsbLJnq i@dora.im
   '';
   home.file."source/nvim".source = config.lib.file.mkOutOfStoreSymlink "${homeDirectory}/.config/nvim";
+  sops.secrets."ssh/id_ed25519" = {
+    sopsFile = osConfig.sops-file.get "common.yaml";
+    path = ".ssh/id_ed25519";
+  };
   home.persistence."/nix/persist/home/tippy" = {
     allowOther = false;
   };
