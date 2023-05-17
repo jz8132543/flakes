db7c9f2ce63f45e60603237e38ea35d73cf0b813
jz8132543 <i@dora.im>
2023-05-17 23:38:31 +0800

    chore: fix traefik

--- nixos/modules/services/traefik.nix
+++ nixos/modules/services/traefik.nix
@@ -1,67 +1,66 @@
 { lib, config, ... }:
 {
-  config = {
-    services.traefik = {
-      enable = true;
-      staticConfigOptions = {
-        experimental.http3 = true;
-        entryPoints = {
-          http = {
-            address = ":80";
-            http.redirections.entryPoint = {
-              to = "https";
-              scheme = "https";
-              permanent = false;
-            };
-          };
-          https = {
-            address = ":443";
-            http.tls.certResolver = "zerossl";
-            http3 = { };
+  config.sops.secrets."traefik/cloudflare" = { };
+  config.services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      experimental.http3 = true;
+      entryPoints = {
+        http = {
+          address = ":80";
+          http.redirections.entryPoint = {
+            to = "https";
+            scheme = "https";
+            permanent = false;
           };
         };
-        certificatesResolvers.zerossl.acme = {
-          caServer = "https://acme.zerossl.com/v2/DV90";
-          email = "blackhole@dora.im";
-          storage = "/var/lib/traefik/acme.json";
-          keyType = "EC256";
-          eab = {
-            kid = builtins.readFile config.sops.secrets."traefik/KID".path;
-            hmacEncoded = builtins.readFile config.sops.secrets."traefik/HMAC".path;
-          };
-          httpChallenge.entrypoint = "http";
-        };
-        ping = {
-          manualRouting = true;
-        };
-        metrics = {
-          prometheus = {
-            addRoutersLabels = true;
-            manualRouting = true;
-          };
+        https = {
+          address = ":443";
+          http.tls.certResolver = "zerossl";
+          http3 = { };
         };
       };
-      dynamicConfigOptions = {
-        tls.options.default = {
-          minVersion = "VersionTLS13";
-          sniStrict = true;
+      certificatesResolvers.zerossl.acme = {
+        caServer = "https://acme.zerossl.com/v2/DV90";
+        email = "blackhole@dora.im";
+        storage = "/var/lib/traefik/acme.json";
+        keyType = "EC256";
+        eab = {
+          kid = "s5QsCWwCNdhUcJAUR1TfNA";
+          hmacEncoded = "kcZnLYZstFNSf1HQQyaBhXWWikJRIxf3pVhgEg_21CiiaF36A4ADzUpt5KpwOzPuOpRCBkNd9oXrhsSirRm2lw";
         };
-        http = {
-          routers = {
-            ping = {
-              rule = "Host(`${config.networking.fqdn}`) && Path(`/`)";
-              entryPoints = [ "https" ];
-              service = "ping@internal";
-            };
-            traefik = {
-              rule = "Host(`${config.networking.fqdn}`) && Path(`/traefik`)";
-              entryPoints = [ "https" ];
-              service = "prometheus@internal";
-            };
+        httpChallenge.entrypoint = "http";
+      };
+      ping = {
+        manualRouting = true;
+      };
+      metrics = {
+        prometheus = {
+          addRoutersLabels = true;
+          manualRouting = true;
+        };
+      };
+    };
+    dynamicConfigOptions = {
+      tls.options.default = {
+        minVersion = "VersionTLS13";
+        sniStrict = true;
+      };
+      http = {
+        routers = {
+          ping = {
+            rule = "Host(`${config.networking.fqdn}`) && Path(`/`)";
+            entryPoints = [ "https" ];
+            service = "ping@internal";
+          };
+          traefik = {
+            rule = "Host(`${config.networking.fqdn}`) && Path(`/traefik`)";
+            entryPoints = [ "https" ];
+            service = "prometheus@internal";
           };
         };
       };
     };
   };
-  systemd.services.traefik.serviceConfig.EnvironmentFile = config.sops.secrets."traefik/cloudflare".path;
+  config.systemd.services.traefik.serviceConfig.EnvironmentFile = config.sops.secrets."traefik/cloudflare".path;
 }
