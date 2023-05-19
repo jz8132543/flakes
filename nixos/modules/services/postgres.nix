3a56fcb87365fedc6017734c367bc124ce6a75c5
jz8132543 <i@dora.im>
2023-05-19 22:35:00 +0800

    chore: test pg

--- nixos/modules/services/postgres.nix
+++ nixos/modules/services/postgres.nix
@@ -8,11 +8,10 @@
   repmgrConfig = ''
     node_id=${self.lib.data.hosts.${config.networking.hostName}.id}
     node_name='${config.networking.hostName}'
-    conninfo='host=${config.networking.hostName}.ts.dora.im user=postgres dbname=repmgr connect_timeout=2'
+    conninfo='host=${config.networking.hostName}.ts.dora.im user=repmgr dbname=repmgr connect_timeout=2'
     data_directory='${config.services.postgresql.dataDir}'
     repmgr_bindir='${repmgr}/bin'
     pg_bindir='${config.services.postgresql.package}/bin'
-    replication_user='postgres'
 
     failover=automatic
     promote_command='${repmgr}/bin/repmgr standby promote -f /etc/repmgr.conf --log-to-file'
@@ -23,35 +22,48 @@
     service_restart_command'sudo systemctl restart postgresql.service'
     service_reload_command'sudo systemctl reload postgresql.service'
   '';
+  postgresHome = "/var/lib/postgresql";
 in {
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
     package = pkgs.postgresql_15;
+    dataDir = "${postgresHome}/${config.services.postgresql.package.psqlSchema}";
+    initdbArgs = ["-E UTF-8 -U postgres --locale=en_US.UTF-8"];
     extraPlugins = with pkgs; [
-      # postgresql15Packages.repmgr
       repmgr
       pgpool
       pgbouncer
     ];
     authentication = ''
       local all all trust
+      host all all 127.0.0.1/32 trust
       host all all 100.64.0.0/10 trust
       host all all fdef:6567:bd7a::/48 trust
+    '';
+    initialScript = pkgs.writeText "initialScript" ''
+      CREATE EXTENSION repmgr;
     '';
     settings = {
       password_encryption = "scram-sha-256";
       hot_standby = "on";
-      wal_level = "replica";
+      wal_level = "logical";
       max_wal_senders = 10;
       max_replication_slots = 10;
-      # wal_keep_segments = 30;
       archive_mode = "on";
       archive_command = "/run/current-system/sw/bin/true";
       # Required for repmgrd
-      # shared_preload_libraries = "repmgr";
+      shared_preload_libraries = "repmgr";
     };
-    ensureDatabases = ["repmgr"];
+    ensureUsers = [
+      {
+        name = "repmgr";
+        ensureClauses = {
+          superuser = true;
+          createdb = true;
+        };
+      }
+    ];
   };
 
   systemd.sockets.repmgr_role = {
@@ -64,7 +76,6 @@ in {
 
   systemd.services."repmgr_role@" = {
     description = "Service for HAProxy to check node status/role";
-    wants = ["repmgr-passwd-key.service"];
     serviceConfig = {
       ExecStart = "-${repmgr}/bin/repmgr -f /etc/repmgr.conf --log-level=ERROR node check --role";
       StandardInput = "socket";
@@ -94,6 +105,20 @@ in {
       mode = "0444";
     };
   };
+  users.users.postgres.createHome = true;
+  users.users.postgres.home = pkgs.lib.mkForce postgresHome;
+  security.sudo.extraRules = [
+    {
+      users = ["postgres"];
+      runAs = "root";
+      commands =
+        map (command: {
+          options = ["NOPASSWD"];
+          command = "/run/current-system/sw/bin/systemctl ${command} postgresql.service";
+        })
+        ["start" "stop" "restart" "reload"];
+    }
+  ];
 
   # backup postgresql database
   # services.postgresqlBackup = {
