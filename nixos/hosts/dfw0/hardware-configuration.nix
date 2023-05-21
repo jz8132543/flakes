8af2f5530c457f2b85ad45dd76591ceb4d38bc6f
jz8132543 <i@dora.im>
2023-05-21 21:36:45 +0800

    chore: disable ssh x11

--- nixos/hosts/dfw0/hardware-configuration.nix
+++ nixos/hosts/dfw0/hardware-configuration.nix
@@ -10,5 +10,6 @@
 
   boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "virtio_scsi" "sr_mod" "virtio_blk" ];
   boot.kernelModules = [ "kvm-intel" ];
+  utils.disk = "/dev/vdb";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
