1cc3943f3eefacb49d848a10d8b6a3fb7a49279a
jz8132543 <i@dora.im>
2023-05-23 23:46:12 +0800

    chore: test stop time

--- nixos/modules/base/tailscale.nix
+++ nixos/modules/base/tailscale.nix
@@ -6,6 +6,8 @@
     allowedUDPPorts = [41641];
   };
 
+  systemd.services.tailscaled.serviceConfig.TimeoutStopSec = "5s";
+
   environment.persistence."/nix/persist" = {
     directories = ["/var/lib/tailscale"];
   };
