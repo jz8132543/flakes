cc8ccfb7924af8814fe9ffadacbbc4df0c4aa802
jz8132543 <i@dora.im>
2023-05-30 22:48:41 +0800

    chore: test nvidia

--- nixos/modules/desktop/nvidia.nix
+++ nixos/modules/desktop/nvidia.nix
@@ -4,10 +4,10 @@
   config,
   ...
 }: {
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (pkgs.lib.getName pkg) [
-      "nvidia-x11"
-    ];
+  # nixpkgs.config.allowUnfreePredicate = pkg:
+  #   builtins.elem (pkgs.lib.getName pkg) [
+  #     "nvidia-x11"
+  #   ];
   services.xserver.videoDrivers = ["nvidia"];
   systemd.services.nvidia-control-devices = {
     wantedBy = [
@@ -21,12 +21,17 @@
   # };
   hardware.nvidia = {
     modesetting.enable = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
     nvidiaSettings = true;
+    nvidiaPersistenced = true;
     prime = {
       offload.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
+    };
+    powerManagement = {
+      enable = true;
+      finegrained = true;
     };
   };
   # environment.systemPackages = with pkgs; [
@@ -35,4 +40,12 @@
   #   glmark2
   #   nvidia-vaapi-driver
   # ];
+  nix.settings = {
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
 }
