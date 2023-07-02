8956ad325e38aa2d682a639ee92460e932df050f
jz8132543 <i@dora.im>
2023-07-02 21:27:41 +0800

    fix: hydra

--- nixos/modules/services/hydra.nix
+++ nixos/modules/services/hydra.nix
@@ -143,6 +143,7 @@ in {
           LogLevel ERROR
           StrictHostKeyChecking no
           Match canonical final Host *.dora.im,*.ts.dora.im
+            Port 1022
             HashKnownHosts no
             UserKnownHostsFile /dev/null
         '';
