6763034a8aef6f384952d287922bab2e828859b0
jz8132543 <i@dora.im>
2023-11-20 11:39:09 +0800

    add: refind

--- nixos/hosts/surface/default.nix
+++ nixos/hosts/surface/default.nix
@@ -1,4 +1,8 @@
-{nixosModules, ...}: {
+{
+  nixosModules,
+  pkgs,
+  ...
+}: {
   imports =
     nixosModules.cloud.all
     ++ nixosModules.users.tippy.all
@@ -12,4 +16,9 @@
     surface-control.enable = true;
     # ipts.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    refind
+    efibootmgr
+  ];
 }
