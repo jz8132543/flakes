5e1a10befdbb2f411e112bf5a077718089735440
jz8132543 <i@dora.im>
2023-12-05 22:54:24 +0800

    vmware: test

--- home-manager/modules/desktop/apps.nix
+++ home-manager/modules/desktop/apps.nix
@@ -11,7 +11,7 @@
     wpsoffice
     # plasma5Packages.kdeconnect-kde
     config.nur.repos.xddxdd.baidupcs-go
-    config.nur.repos.xddxdd.wechat-uos
+    # config.nur.repos.xddxdd.wechat-uos
   ];
   home.global-persistence = {
     directories = [
