e7d70b81c8a8bfdadc2470358cbd8920520b2177
jz8132543 <i@dora.im>
2024-03-08 15:59:13 +0800

    fix: isk

--- nixos/modules/base/environment/isNAT/default.nix
+++ nixos/modules/base/environment/isNAT/default.nix
@@ -2,51 +2,50 @@
   lib,
   config,
   ...
-}: let
-  cfg = config.services.traefik.dynamicConfigOptions.http.routers;
-in
-  with lib; {
-    options.environment = {
-      isNAT = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to enable NAT mode.
-        '';
-      };
-      altHTTPS = mkOption {
-        type = types.int;
-        default = 8443;
-        description = ''
-          The port of https alt
-        '';
-      };
-      altHTTP = mkOption {
-        type = types.int;
-        default = 8080;
-        description = ''
-          The port of http alt
-        '';
-      };
+}:
+with lib; {
+  options.environment = {
+    isNAT = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable NAT mode.
+      '';
     };
-    config.networking =
-      if config.environment.isNAT
-      then {
-        nftables.ruleset = ''
-          table ip nat {
-            chain prerouting {
-              type nat hook prerouting priority 0; policy accept;
-              tcp dport ${toString config.environment.altHTTP} redirect to 80
-              tcp dport ${toString config.environment.altHTTPS} redirect to 443
-            }
-
-            chain postrouting {
-              type nat hook postrouting priority 0; policy accept;
-            }
+    altHTTPS = mkOption {
+      type = types.int;
+      default = 8443;
+      description = ''
+        The port of https alt
+      '';
+    };
+    altHTTP = mkOption {
+      type = types.int;
+      default = 8080;
+      description = ''
+        The port of http alt
+      '';
+    };
+  };
+  config.networking =
+    if config.environment.isNAT
+    then {
+      nftables.ruleset = ''
+        table ip nat {
+          chain prerouting {
+            type nat hook prerouting priority 0; policy accept;
+            tcp dport ${toString config.environment.altHTTP} redirect to 80
+            tcp dport ${toString config.environment.altHTTPS} redirect to 443
+            udp dport ${toString config.environment.altHTTPS} redirect to 443
           }
-        '';
-        firewall.allowedTCPPorts = with config.environment; [altHTTPS altHTTP];
-        firewall.allowedUDPPorts = with config.environment; [altHTTPS];
-      }
-      else {};
-  }
+
+          chain postrouting {
+            type nat hook postrouting priority 0; policy accept;
+          }
+        }
+      '';
+      firewall.allowedTCPPorts = with config.environment; [altHTTPS altHTTP];
+      firewall.allowedUDPPorts = with config.environment; [altHTTPS];
+    }
+    else {};
+}
