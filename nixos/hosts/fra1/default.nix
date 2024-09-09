f841d0999b267573b2b73b9455bd6b3271670994
jz8132543 <i@dora.im>
2024-09-09 13:38:23 +0800

    fix: fra1

--- nixos/hosts/fra1/default.nix
+++ nixos/hosts/fra1/default.nix
@@ -4,16 +4,16 @@
     ++ nixosModules.users.tippy.all
     ++ [
       ./hardware-configuration.nix
-      #nixosModules.services.traefik
-      #nixosModules.services.headscale
-      #nixosModules.services.derp
-      #nixosModules.services.proxy
-      #nixosModules.services.tuic
-      #nixosModules.services.postgres
-      #nixosModules.services.doraim
-      #nixosModules.services.ntfy
-      #(import nixosModules.services.keycloak {})
-      #(import nixosModules.services.vaultwarden {})
-      #(import nixosModules.services.alist {})
+      nixosModules.services.traefik
+      nixosModules.services.headscale
+      nixosModules.services.derp
+      nixosModules.services.proxy
+      nixosModules.services.tuic
+      nixosModules.services.postgres
+      nixosModules.services.doraim
+      nixosModules.services.ntfy
+      (import nixosModules.services.keycloak {})
+      (import nixosModules.services.vaultwarden {})
+      (import nixosModules.services.alist {})
     ];
 }
