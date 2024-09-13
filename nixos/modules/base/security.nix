b1eb3ae1ffba0fdc2144757eca7c05d52a78ae49
jz8132543 <i@dora.im>
2024-09-14 00:03:50 +0800

    fix: mihomo

--- nixos/modules/base/security.nix
+++ nixos/modules/base/security.nix
@@ -5,11 +5,11 @@
   #   enable = true;
   #   # execWheelOnly = true;
   # };
-  security.sudo-rs = {
-    enable = true;
-    execWheelOnly = true;
-    wheelNeedsPassword = false;
-  };
+  # security.sudo-rs = {
+  #   enable = true;
+  #   execWheelOnly = true;
+  #   wheelNeedsPassword = false;
+  # };
   # security.pki.certificateFiles = ["${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"];
   security.polkit.enable = true;
   boot.blacklistedKernelModules = [ "virtio_balloon" ];
--- nixos/modules/desktop/apps.nix
+++ nixos/modules/desktop/apps.nix
@@ -6,6 +6,16 @@
 }:
 {
   imports = [ nixosModules.services.aria2 ];
+  services.mihomo = {
+    enable = true;
+    tunMode = true;
+    webui = pkgs.metacubexd;
+    configFile = "/etc/mihomo/config.yaml";
+  };
+  # systemd.services.mihomo.serviceConfig.ExecStartPre = [
+  #   "${pkgs.coreutils}/bin/ln -sf ${pkgs.v2ray-geoip}/share/v2ray/geoip.dat /var/lib/private/mihomo/GeoIP.dat"
+  #   "${pkgs.coreutils}/bin/ln -sf ${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat /var/lib/private/mihomo/GeoSite.dat"
+  # ];
   programs = {
     # clash-verge = {
     #   enable = true;
@@ -41,9 +51,14 @@
   networking.firewall.allowedTCPPorts = [
     config.ports.qrcp
   ];
-  environment.global-persistence.user = {
+  environment.global-persistence = {
     directories = [
-      ".config/clash-verge"
+      "/etc/mihomo"
     ];
+    user = {
+      directories = [
+        ".config/clash-verge"
+      ];
+    };
   };
 }
