c2b1899ac1fff4ee804a35923979b3590b35b047
jz8132543 <i@dora.im>
2024-11-20 00:15:22 +0800

    fix: derper

--- nixos/modules/base/nixpkgs.nix
+++ nixos/modules/base/nixpkgs.nix
@@ -21,10 +21,17 @@ let
         inherit (inputs'.nix-gc-s3.packages) nix-gc-s3;
         # inherit (inputs'.headscale.packages) headscale;
         clash2sing-box = inputs'.clash2sing-box.packages.default;
-        # tailscale = prev.tailscale.overrideAttrs (old: {
-        #   # tailscale = inputs'.tailscale.packages.tailscale.overrideAttrs (old: {
-        #   subPackages = old.subPackages ++ [ "cmd/derper" ] ++ [ "cmd/derpprobe" ];
-        # });
+        tailscale = prev.tailscale.overrideAttrs (_old: {
+          # tailscale = inputs'.tailscale.packages.tailscale.overrideAttrs (old: {
+          # subPackages = old.subPackages ++ [ "cmd/derper" ] ++ [ "cmd/derpprobe" ];
+          subPackages = [
+            "cmd/tailscale"
+            "cmd/tailscaled"
+            "cmd/derper"
+            "cmd/stunc"
+            "cmd/hello"
+          ];
+        });
       }
     )
   ];
