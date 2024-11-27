019b2056fd2f251a18618f5f844c150d47d15fc8
jz8132543 <i@dora.im>
2024-11-28 01:22:51 +0800

    add: jellyfin

--- nixos/modules/services/jellyfin.nix
+++ nixos/modules/services/jellyfin.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   portNumber = 8096;
 in
@@ -21,4 +21,44 @@ in
       };
     };
   };
+  # systemd.tmpfiles.rules = [
+  #   # "d '/mnt/alist' 0077 jellyfin jellyfin - -"
+  #   "d '/mnt/jellyfin/alist' 0777 jellyfin jellyfin - -"
+  #   "d '/var/empty/.cache/rclone/' 0775 root root - -"
+  # ];
+  systemd.services.mount-alist = {
+    after = [ "network-online.target" ];
+    serviceConfig = {
+      User = "root";
+      Type = "notify";
+      Restart = "on-failure";
+      # ExecStartPre = "/usr/bin/env mkdir -p /var/cache/jellyfin/mount-alist";
+      ExecStart = ''
+        ${pkgs.rclone}/bin/rclone \
+        --allow-other=true \
+        --config=${config.sops.templates."mount-alist".path} \
+        --vfs-cache-mode writes \
+        --ignore-checksum \
+        mount alist: /mnt/alist -vv
+      '';
+      ExecStop = "/bin/fusermount -u /mnt/alist";
+    };
+    wantedBy = [ "default.target" ];
+  };
+  sops.templates."mount-alist" = {
+    owner = "jellyfin";
+    content = ''
+      [alist]
+      type = webdav
+      url = https://alist.${config.networking.domain}/dav
+      vendor = rclone
+      user = ${config.sops.placeholder."alist/app/username"}
+      pass = ${config.sops.placeholder."alist/app/password-rclone"}
+    '';
+  };
+  sops.secrets = {
+    "alist/app/username" = { };
+    "alist/app/password-rclone" = { };
+  };
+  systemd.packages = with pkgs; [ rclone ];
 }
