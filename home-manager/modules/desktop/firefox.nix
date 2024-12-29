3d851a26367286df4c4ce75081a5f899e723f0c7
jz8132543 <i@dora.im>
2024-12-30 02:24:53 +0800

    update: firefox

--- home-manager/modules/desktop/firefox.nix
+++ home-manager/modules/desktop/firefox.nix
@@ -24,10 +24,10 @@
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
         };
-        "switchyomega@feliscatus.addons.mozilla.org" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/switchyomega/latest.xpi";
-        };
+        # "switchyomega@feliscatus.addons.mozilla.org" = {
+        #   installation_mode = "force_installed";
+        #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/switchyomega/latest.xpi";
+        # };
         "uBlock0@raymondhill.net" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
