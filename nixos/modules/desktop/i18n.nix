4a60fa05325cc76d868b85d72b326fd5c787ae70
jz8132543 <i@dora.im>
2026-02-28 01:21:43 +0800

    test: fcitx5

---
+++ nixos/modules/desktop/i18n.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-rime
+        librime
+        librime-lua
+        librime-octagram
+      ];
+      waylandFrontend = true;
+    };
+  };
+}
