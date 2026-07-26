a16bf5e7720ce2a9db1da91ab670b6934b263e0e
jz8132543 <i@dora.im>
2026-07-26 17:36:10 +0800

    fix: ext4

--- nixos/modules/optimize/ext4.nix
+++ nixos/modules/optimize/ext4.nix
@@ -1,68 +1,17 @@
 {
-  config,
   lib,
-  inputs,
   ...
 }:
 {
-  imports = [
-    inputs.disko.nixosModules.disko
-    ../cloud/disko-image-builder.nix
-  ];
-
-  disko.enableConfig = true;
-  disko.devices.disk.main = lib.mkForce {
-    imageSize = "6G";
-    type = "disk";
-    device = "${config.utils.disk}";
-    content = {
-      type = "gpt";
-      partitions = {
-        BIOS = {
-          label = "BIOS";
-          size = "1M";
-          type = "EF02";
-        };
-        EFI = {
-          label = "EFI";
-          size = "200M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot/efi";
-          };
-        };
-        NIXOS = {
-          label = "NIXOS";
-          end = "-0";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-            mountOptions = [ "noatime" ];
-          };
-        };
-      };
-    };
+  disko.devices.disk.main.content.partitions.NIXOS.content = lib.mkForce {
+    type = "filesystem";
+    format = "ext4";
+    mountpoint = "/";
+    mountOptions = [ "noatime" ];
   };
 
   boot.supportedFilesystems = [
     "ext4"
     "vfat"
   ];
-
-  boot = {
-    growPartition = lib.mkDefault true;
-    loader = {
-      timeout = 2;
-      efi.efiSysMountPoint = "/boot/efi";
-      grub = {
-        enable = true;
-        device = "${config.utils.disk}";
-        efiSupport = lib.mkDefault true;
-        efiInstallAsRemovable = lib.mkDefault true;
-      };
-    };
-  };
 }
