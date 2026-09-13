94d570f841b8a97a673dec25b7784bcc9c42a9ab
jz8132543 <i@dora.im>
2026-09-13 20:22:44 +0800

    fix: grub

--- nixos/modules/desktop/boot.nix
+++ nixos/modules/desktop/boot.nix
@@ -4,9 +4,27 @@
   ...
 }:
 let
-  catppuccinGrub = pkgs.catppuccin-grub.override {
+  catppuccinGrubBase = pkgs.catppuccin-grub.override {
     flavor = "mocha";
   };
+  fontSize = 18;
+  fontTtf = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-SemiBold.ttf";
+  catppuccinGrub =
+    pkgs.runCommand "catppuccin-grub-mocha-jetbrains"
+      {
+        nativeBuildInputs = [ pkgs.grub2 ];
+      }
+      ''
+        mkdir -p "$out"
+        cp -r ${catppuccinGrubBase}/* "$out/"
+        chmod -R u+w "$out"
+
+        # 使用 grub-mkfont 从 JetBrains Mono Nerd Font 矢量字体转换生成高清 pf2 字体（含 NixOS、Windows 等图标）
+        grub-mkfont --size=${toString fontSize} --name="JetBrainsMono NF" "${fontTtf}" -o "$out/font.pf2"
+
+        # 将主题配置文件中的字体从粗糙的单色点阵 Unifont 替换为 JetBrains Mono Nerd Font
+        sed -i "s/Unifont Regular 16/JetBrainsMono NF Regular ${toString fontSize}/g" "$out/theme.txt"
+      '';
 in
 {
   boot = {
@@ -15,7 +33,7 @@ in
 
     # GRUB 引导配置
     loader.grub = {
-      # 启用 Catppuccin Mocha 主题
+      # 启用 Catppuccin Mocha 主题（搭配 JetBrains Mono Nerd Font 高清矢量字体）
       theme = lib.mkDefault catppuccinGrub;
       font = lib.mkDefault "${catppuccinGrub}/font.pf2";
 
@@ -23,17 +41,6 @@ in
       # 设置适宜分辨率（如 1024x768），由硬件/UEFI 缩放显示，使文字与图标比例正常
       gfxmodeEfi = lib.mkDefault "1024x768,auto";
       gfxmodeBios = lib.mkDefault "1024x768";
-    };
-
-    # 启用 Plymouth 开机动画，仅配置 Catppuccin Mocha 主题
-    plymouth = {
-      enable = true;
-      theme = "catppuccin-mocha";
-      themePackages = [
-        (pkgs.catppuccin-plymouth.override {
-          variant = "mocha";
-        })
-      ];
     };
   };
 }
